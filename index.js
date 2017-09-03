8aa2ddccea06e4703bc244ab4037bee9298ae0f4
Max Moawad <maxmoawad1@Maxs-MacBook-Pro.local>
2017-09-03 10:28:20 -0400

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const APIAI_TOKEN = "ef171aa6f9d34be384da413be9c9eb47";
+const APIAI_SESSION_ID = "cat";
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // js, css, images
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+const io = require('socket.io')(server);
+const apiai = require('apiai')(APIAI_TOKEN);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+// Web UI
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket) {
+  socket.on('chat message', (text) => {
+    console.log('Message: ' + text);
+
+    // Reply from API.ai
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: APIAI_SESSION_ID
+    });
+
+    apiaiReq.on('response', (response) => {
+      let aiText = response.result.fulfillment.speech;
+
+      console.log('Bot reply: ' + aiText);
+      socket.emit('bot reply', aiText);
+    });
+
+    apiaiReq.on('error', (error) => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+
+  });
+});
